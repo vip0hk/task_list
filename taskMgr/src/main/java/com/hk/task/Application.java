2c137891029b66d178c8cddf373b14930544a8e1
vip0_hk@163 <vip0_hk@163.com>
2017-04-26 13:13:25 +0800

    session timeout

--- taskMgr/src/main/java/com/hk/task/Application.java
+++ taskMgr/src/main/java/com/hk/task/Application.java
@@ -3,6 +3,9 @@ package com.hk.task;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,5 +30,15 @@ public class Application extends WebMvcConfigurerAdapter {
     public ModelAndView index() {
         ModelAndView result = new ModelAndView("index");
         return result;
+    }
+    
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+           return new EmbeddedServletContainerCustomizer() {
+               @Override
+               public void customize(ConfigurableEmbeddedServletContainer container) {
+                    container.setSessionTimeout(60*60*8);//单位为S
+              }
+        };
     }
 }
